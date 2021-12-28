978333f8ac3a83da9ae5c2c2c430d62b6a194b42
renzien <alifrizki1011@gmail.com>
2021-12-28 18:41:34 +0700

    add Page

---
+++ Script.js
@@ -0,0 +1,9 @@
+var a = prompt("Masukin Usernamenyaa OwO)/");
+var b = prompt("Masukin Passwordnya Juga OwO)/");
+
+if (a == "Chiiya" && b == "Rilxa"){
+    window.location.href="start.html";
+} else {
+    alert("Invalid Details");
+    window.location.href="index.html";
+}
